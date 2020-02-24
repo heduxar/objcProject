e75d7991e0f7070c66a94cc9f8f4df3a6cd17e61
Yuriy Sultanov <>
2020-02-24 16:22:05 +0500

    lesson1

---
+++ ObjcProject/UIComponentsVC.h
@@ -0,0 +1,17 @@
+//
+//  UIComponentsVC.h
+//  ObjcProject
+//
+//  Created by Юрий Султанов on 24.02.2020.
+//  Copyright © 2020 Yuriy Sultanov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIComponentsVC : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
